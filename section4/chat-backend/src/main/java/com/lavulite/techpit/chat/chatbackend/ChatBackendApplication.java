b2c4f6472dff2d87e403ecdb00ca6f6ebeb4de1a
Lavulite <s.sugiyama0811@gmail.com>
2022-07-31 18:29:59 +0900

    section3完成

---
+++ section4/chat-backend/src/main/java/com/lavulite/techpit/chat/chatbackend/ChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lavulite.techpit.chat.chatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBackendApplication.class, args);
+	}
+
+}
--- section4/chat-backend/src/main/java/com/lavulite/techpit/chat/chatbackend/app/controller/HelloController.java
+++ section4/chat-backend/src/main/java/com/lavulite/techpit/chat/chatbackend/app/controller/HelloController.java
@@ -1,22 +1,16 @@
 package com.lavulite.techpit.chat.chatbackend.app.controller;
 
-import java.util.Optional;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.lavulite.techpit.chat.chatbackend.domain.hello.model.Hello;
 
 @RestController
 @RequestMapping()
 public class HelloController {
   
   @GetMapping("/hello")
-  public Hello hello(@RequestParam("name") Optional<String> name) {
-    String resName = name.orElse("world!");
-    return new Hello("Hello, " + resName);
+  public String hello() {
+    return "Hello, world!";
   }
 
 }
