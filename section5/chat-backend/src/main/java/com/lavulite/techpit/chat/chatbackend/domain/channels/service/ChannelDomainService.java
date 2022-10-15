594e884b7a892a89331e3a69afe2e755b00214b1
Lavulite <s.sugiyama0811@gmail.com>
2022-10-15 12:48:45 +0900

    section4完成

---
+++ section5/chat-backend/src/main/java/com/lavulite/techpit/chat/chatbackend/domain/channels/service/ChannelDomainService.java
@@ -0,0 +1,33 @@
+package com.lavulite.techpit.chat.chatbackend.domain.channels.service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.lavulite.techpit.chat.chatbackend.domain.channels.model.Channel;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ChannelDomainService {
+
+  private final ChannelRepository channelRepository;
+
+  public Channel create(Channel channel){
+    // ユーザからは登録用のIDを受け取らない仕様のため、DB内にあるIDの最大値+1を新しいチャンネルのIDとする。
+    var currentMaxId = channelRepository.getMaxId();
+    var newid = currentMaxId.orElse(0) + 1;
+    channel.setId(newid);
+
+    // DBに永続化する。
+    channelRepository.insert(channel);
+    return channel;
+  }
+  
+  public List<Channel> findAll(){
+    return channelRepository.findAll();
+  }
+}
