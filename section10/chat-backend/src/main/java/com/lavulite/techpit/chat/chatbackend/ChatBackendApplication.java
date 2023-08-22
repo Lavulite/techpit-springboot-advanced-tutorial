3139451cf91e62a3073f9f35c5cc783c48d53e54
Lavulite <lavulite.eng@gmail.com>
2023-08-22 22:27:51 +0900

    不要な条件を削除

--- section10/chat-backend/src/main/java/com/lavulite/techpit/chat/chatbackend/ChatBackendApplication.java
+++ section10/chat-backend/src/main/java/com/lavulite/techpit/chat/chatbackend/ChatBackendApplication.java
@@ -1,5 +1,7 @@
 package com.lavulite.techpit.chat.chatbackend;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
